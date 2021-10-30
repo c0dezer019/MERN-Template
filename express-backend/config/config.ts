81dc16974b38f82e983095cdcdcb47bcdff44f06
c0dezer019 <c0dezer019@bblanke nship.me>
2021-10-29 22:56:06 -0500

    Configured MongoClient.

--- express-backend/config/config.ts
+++ express-backend/config/config.ts
@@ -1,17 +1,15 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
-const USERNAME = process.env.username;
-const PASSWORD = process.env.password;
-const CLUSTER = process.env.cluster;
-const DB = process.env.prim_db;
+type MongoUrl = `mongodb://${string}:${string}@${string}.nrozi.mongodb.net/${string}?${string}`
 
-const db = process.env.db;
-const collection = '';
+const username: string = encodeURIComponent(process.env.username);
+const password: string = encodeURIComponent(process.env.password);
+const cluster: string = process.env.cluster;
+const db: string = process.env.prim_db;
 
-const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@${CLUSTER}.nrozi.mongodb.net/${DB}?retryWrites=true&w=majority`;
-mongoose.connect(uri);
+const authMechanism = "DEFAULT";
 
-const Cat = mongoose.model('Cat', { name: String });
+const uri: MongoUrl = `mongodb://${username}:${password}@${cluster}.nrozi.mongodb.net/${db}?authMechanism=${authMechanism}`;
 
-const main = async () => {};
+export const client = new MongoClient(uri);
